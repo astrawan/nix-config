b1b07ba553d8b77cffda348c8afeaad97ce35e58
Astrawan Wayan <astra@pm.me>
2026-01-30 21:17:20 +0800

    feat(pkgs): upgrade gvfs.yazi

--- pkgs/yazi/plugins/gvfs.nix
+++ pkgs/yazi/plugins/gvfs.nix
@@ -5,19 +5,23 @@
 }:
 stdenvNoCC.mkDerivation {
   pname = "gvfs.yazi";
-  version = "0-unstable-2026-01-07";
+  version = "0-unstable-2026-01-26";
 
   src = fetchFromGitHub {
     owner = "boydaihungst";
     repo = "gvfs.yazi";
-    rev = "d9e3e0d3ce352f230ec066c116afdb496a114530";
-    hash = "sha256-YhiqH3fFX6XjxyoyWwz+VpyHmFWGegWRie/GHeD6+SE=";
+    rev = "30440c70affb1916a553d805a193c4b5ddab4058";
+    hash = "sha256-jcNHthObhZ5OPHwBxy0Pc3udlGUaZF9WqxHeoXeFbtg=";
   };
 
   installPhase = ''
     mkdir -p $out
 
     cp -r * $out/
+
+    rm -f $out/*.md
+    rm -f $out/.luarc.json
+    rm -f $out/assets/gnome-keyring-daemon.service
   '';
 
   meta = {
