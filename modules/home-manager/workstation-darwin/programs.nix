b3b89cb77ca9e3a55f35bb8274f4e7954b7f3304
Astrawan Wayan <astra@pm.me>
2026-01-31 23:37:33 +0800

    feat(darwin): homebrew fish integration

--- modules/home-manager/workstation-darwin/programs.nix
+++ modules/home-manager/workstation-darwin/programs.nix
@@ -9,6 +9,10 @@
     programs.mpv.enable = true;
 
     devlive.programs.eza.enable = true;
+    # Enable homebrew integration
+    devlive.programs.fish.interactiveShellInit = ''
+      [ -f "/opt/homebrew/bin/brew" ] && eval "$(/opt/homebrew/bin/brew shellenv fish)"
+    '';
     devlive.programs.wezterm = lib.mkIf (config.devlive.programs.wezterm.enable) {
       settings = {
         macos_window_background_blur = 60;
