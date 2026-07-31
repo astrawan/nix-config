142d2d2771b80a4162213714f664aa71a213708b
Astrawan Wayan <astra@pm.me>
2026-07-31 18:31:15 +0800

    feat(home-manager): noctalia-5 adjust settings and code formatting

--- modules/home-manager/workstation-linux/features/desktop/noctalia/noctalia.nix
+++ modules/home-manager/workstation-linux/features/desktop/noctalia/noctalia.nix
@@ -1,39 +1,50 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   desktop = config.devlive.features.desktop;
 in
 {
   config = lib.mkIf (desktop.type == "noctalia") {
-    home.packages = with pkgs; [
-      adw-gtk3
-      adwaita-icon-theme
-      bazaar
-      # noctalia gtk4 color schema integration
-      glib
-      jellyfin-tui
-      kdePackages.breeze-icons
-      kdePackages.qt6ct
-      libsForQt5.qt5ct
-      networkmanagerapplet
-      cliphist
-      wl-clipboard
-      wl-screenrec
-      wl-mirror
-    ] ++desktop.extraHomePackages ++desktop.noctalia.extraHomePackages ++(
-      if desktop.noctalia.package == pkgs.noctalia-shell-5 then
-        [ desktop.noctalia.package ]
-      else
-        [ python3 ]
-    ) ++(
-      if desktop.noctalia.compositor == "niri" then
-        [
-          pkgs.xdg-desktop-portal-gnome
-          pkgs.nautilus
-        ]
-      else
-        [ ]
-    );
+    home.packages =
+      with pkgs;
+      [
+        adw-gtk3
+        adwaita-icon-theme
+        bazaar
+        # noctalia gtk4 color schema integration
+        glib
+        jellyfin-tui
+        kdePackages.breeze-icons
+        kdePackages.qt6ct
+        libsForQt5.qt5ct
+        networkmanagerapplet
+        cliphist
+        wl-clipboard
+        wl-screenrec
+        wl-mirror
+      ]
+      ++ desktop.extraHomePackages
+      ++ desktop.noctalia.extraHomePackages
+      ++ (
+        if desktop.noctalia.package == pkgs.noctalia-shell-5 then
+          [ desktop.noctalia.package ]
+        else
+          [ python3 ]
+      )
+      ++ (
+        if desktop.noctalia.compositor == "niri" then
+          [
+            pkgs.xdg-desktop-portal-gnome
+            pkgs.nautilus
+          ]
+        else
+          [ ]
+      );
     home.pointerCursor = {
       enable = true;
       gtk.enable = true;
@@ -42,7 +53,7 @@ in
       name = "macOS";
       size = 32;
     };
-    home.file.".config/qt6ct/qt6ct.conf".text = lib.generators.toINI {} {
+    home.file.".config/qt6ct/qt6ct.conf".text = lib.generators.toINI { } {
       Appearance = {
         color_scheme_path = "${config.xdg.configHome}/qt6ct/colors/noctalia.conf";
         custom_palette = true;
@@ -53,7 +64,7 @@ in
         general = ''"DejaVu Sans,11,-1,5,500,0,0,0,0,0,0,0,0,0,0,1,Regular"'';
       };
     };
-    home.file.".config/qt5ct/qt5ct.conf".text = lib.generators.toINI {} {
+    home.file.".config/qt5ct/qt5ct.conf".text = lib.generators.toINI { } {
       Appearance = {
         color_scheme_path = "${config.xdg.configHome}/qt5ct/colors/noctalia.conf";
         custom_palette = true;
@@ -64,9 +75,11 @@ in
         general = ''"DejaVu Sans,11,-1,5,50,0,0,0,0,0,Regular"'';
       };
     };
-    home.file.".config/systemd/user/xdg-desktop-portal-gnome.service" = lib.mkIf (desktop.noctalia.compositor == "niri") {
-      source = "${pkgs.xdg-desktop-portal-gnome}/share/systemd/user/xdg-desktop-portal-gnome.service";
-    };
+    home.file.".config/systemd/user/xdg-desktop-portal-gnome.service" =
+      lib.mkIf (desktop.noctalia.compositor == "niri")
+        {
+          source = "${pkgs.xdg-desktop-portal-gnome}/share/systemd/user/xdg-desktop-portal-gnome.service";
+        };
     devlive.programs.eza.enable = true;
     devlive.programs.niri = lib.mkIf (desktop.noctalia.compositor == "niri") {
       enable = true;
@@ -147,21 +160,9 @@ in
                   "opensnitch-ui"
                   "udiskie"
                 ]
-                ++ (
-                  if config.devlive.services.tailscale.enable then
-                    [ "systray_*" ]
-                  else []
-                )
-                ++ (
-                    if desktop.enableTelegram then
-                      [ "Telegram Desktop" ]
-                    else []
-                )
-                ++ (
-                    if desktop.enableWhatsApp then
-                      [ "ZapZap" ]
-                    else []
-                );
+                ++ (if config.devlive.services.tailscale.enable then [ "systray_*" ] else [ ])
+                ++ (if desktop.enableTelegram then [ "Telegram Desktop" ] else [ ])
+                ++ (if desktop.enableWhatsApp then [ "ZapZap" ] else [ ]);
               }
               {
                 id = "NotificationHistory";
@@ -222,7 +223,7 @@ in
           compactLockScreen = true;
           enableShadows = false;
           lockScreenMonitors = [ "eDP-1" ];
-          lockScreenBlur =  1;
+          lockScreenBlur = 1;
           showScreenCorners = true;
           showSessionButtonsOnLockScreen = false;
         };
@@ -285,240 +286,237 @@ in
         };
       };
     };
-    home.file.".config/noctalia/settings.toml" = lib.mkIf (desktop.noctalia.package == pkgs.noctalia-shell-5) {
-      source = (pkgs.formats.toml {}).generate "noctalia-shell-5-settings.toml" {
-        bar.default = {
-          background_opacity = 0.8;
-          border_width = 1.0;
-          center = [
-            "cpu"
-            "temp"
-            "ram"
-            "network_rx"
-            "network_tx"
-          ];
-          concave_edge_corners = false;
-          margin_edge = 0;
-          margin_ends = 16;
-          position = "left";
-          radius = 18;
-          radius_bottom_left = 0;
-          radius_top_left = 0;
-          start = [
-            "launcher"
-            "clock"
-            "date"
-            "wallpaper"
-            "workspaces"
-          ];
-          thickness = 48;
-          widget_spacing = 8;
-          monitor."HDMI-A-1".enabled = false;
-        };
-
-        battery = {
-          warning_threshold = 20;
-        };
-
-        calendar = {
-          enabled = true;
-          account.personal_google = {
-            type = "google";
-          };
-        };
-
-        control_center = {
-          sidebar = "full";
-          sidebar_section = "full";
-        };
-
-        dock = {
-          auto_hide = false;
-          background_opacity = 0.8;
-          border_width = 1.0;
-          concave_edge_corners = false;
-          enabled = true;
-          margin_edge = 16;
-          monitors = [ "eDP-1" ];
-          position = "right";
-          reserve_space = false;
-          show_dots = true;
-          smart_auto_hide = true;
-        };
-
-        idle = {
-          behavior_order = [
-            "lock"
-            "screen-off"
-            "suspend"
-          ];
-          behavior = {
-            lock = {
-              action = "lock";
-              enabled = true;
-              timeout = 600;
+    home.file.".config/noctalia/settings.toml" =
+      lib.mkIf (desktop.noctalia.package == pkgs.noctalia-shell-5)
+        {
+          source = (pkgs.formats.toml { }).generate "noctalia-shell-5-settings.toml" {
+            bar.default = {
+              background_opacity = 0.8;
+              border_width = 1.0;
+              center = [
+                "cpu"
+                "temp"
+                "ram"
+                "network_rx"
+                "network_tx"
+              ];
+              concave_edge_corners = false;
+              end = [
+                "launcher"
+                "clock"
+                "date"
+                "wallpaper"
+                "workspaces"
+              ];
+              margin_edge = 0;
+              margin_ends = 16;
+              position = "left";
+              radius = 18;
+              radius_bottom_left = 0;
+              radius_top_left = 0;
+              start = [
+                "launcher"
+                "clock"
+                "date"
+                "wallpaper"
+                "workspaces"
+              ];
+              thickness = 48;
+              widget_spacing = 8;
+              monitor."HDMI-A-1".enabled = false;
             };
-            screen-off = {
-              action = "screen_off";
-              enabled = true;
-              timeout = 660;
+
+            battery = {
+              warning_threshold = 20;
             };
-            suspend = {
-              action = "suspend";
+
+            calendar = {
               enabled = true;
-              lock_before_suspend = true;
-              timeout = 900;
+              account.personal_google = {
+                type = "google";
+              };
+            };
+
+            control_center = {
+              sidebar = "full";
+              sidebar_section = "full";
+            };
+
+            dock = {
+              auto_hide = false;
+              background_opacity = 0.8;
+              border_width = 1.0;
+              concave_edge_corners = false;
+              enabled = true;
+              margin_edge = 16;
+              monitors = [ "eDP-1" ];
+              position = "right";
+              reserve_space = false;
+              show_dots = true;
+              smart_auto_hide = true;
+            };
+
+            idle = {
+              behavior_order = [
+                "lock"
+                "screen-off"
+                "suspend"
+              ];
+              behavior = {
+                lock = {
+                  action = "lock";
+                  enabled = true;
+                  timeout = 600;
+                };
+                screen-off = {
+                  action = "screen_off";
+                  enabled = true;
+                  timeout = 660;
+                };
+                suspend = {
+                  action = "suspend";
+                  enabled = true;
+                  lock_before_suspend = true;
+                  timeout = 900;
+                };
+              };
+            };
+
+            lockscreen = {
+              fingerprint = false;
+              monitors = [ "eDP-1" ];
+            };
+
+            notification = {
+              background_opacity = 0.8;
+              monitors = [ "eDP-1" ];
+              offset_y = 16;
+              position = "bottom_left";
+            };
+
+            osd = {
+              background_opacity = 0.8;
+              lock_keys = false;
+              monitors = [ "eDP-1" ];
+              offset_y = 16;
+              orientation = "horizontal";
+              position = "top_center";
+            };
+
+            shell = {
+              font_family = "Adwaita Mono";
+
+              panel = {
+                attach_clipboard = false;
+                attach_control_center = false;
+                attach_launcher = false;
+                attach_session = false;
+                attach_wallpaper = false;
+                clipboard_placement = "auto";
+                control_center_placement = "floating";
+                floating_offset = 16;
+                launcher_position = "auto";
+                polkit_position = "auto";
+                session_placement = "floating";
+                transparency_mode = "soft";
+                wallpaper_placement = "floating";
+              };
+
+              shadow = {
+                alpha = 1.0;
+                direction = "center";
+              };
+            };
+
+            theme = {
+              mode = "dark";
+              source = "wallpaper";
+              wallpaper_scheme = "m3-content";
+
+              templates = {
+                builtin_ids = [
+                  "gtk3"
+                  "gtk4"
+                  "kcolorscheme"
+                  "qt"
+                ]
+                ++ (if (config.programs.wezterm.enable) then [ "wezterm" ] else [ ])
+                ++ (
+                  if (desktop.noctalia.compositor == "hyprland") then
+                    [ "hyprland" ]
+                  else if (desktop.noctalia.compositor == "niri") then
+                    [ "niri" ]
+                  else
+                    [ ]
+                );
+                community_ids =
+                  (if (desktop.enableTelegram) then [ "telegram" ] else [ ])
+                  ++ (if (config.devlive.programs.vscode.enable) then [ "vscode" ] else [ ])
+                  ++ (if (config.devlive.programs.yazi.enable) then [ "yazi" ] else [ ])
+                  ++ (if (config.devlive.programs.zathura.enable) then [ "zathura" ] else [ ])
+                  ++ (if (config.devlive.programs.zed-editor.enable) then [ "zed" ] else [ ]);
+              };
+            };
+
+            wallpaper = {
+              directory = "~/Pictures/Wallpapers";
+
+              default = {
+                path = "${config.home.homeDirectory}/Pictures/Wallpapers/wallhaven_j38o75.jpg";
+              };
+            };
+
+            weather = {
+              auto_locate = true;
+            };
+
+            widget = {
+              control-center = {
+                custom_image = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake-white.svg";
+                custom_image_colorize = true;
+                glyph = "snowflake";
+                scale = 1.5;
+              };
+              cpu = {
+                show_label = false;
+                show_value = false;
+              };
+              network = {
+                show_label = false;
+                show_value = false;
+              };
+              network_rx = {
+                show_label = false;
+                show_value = false;
+              };
+              network_tx = {
+                show_label = false;
+                show_value = false;
+              };
+              ram = {
+                show_label = false;
+                show_value = false;
+              };
+              temp = {
+                show_label = false;
+                show_value = false;
+              };
+              workspaces = {
+                capsule_padding = 8.0;
+              };
             };
           };
         };
-
-        lockscreen = {
-          fingerprint = false;
-          monitors = [ "eDP-1" ];
-        };
-
-        notification = {
-          background_opacity = 0.8;
-          monitors = [ "eDP-1" ];
-          offset_y = 16;
-          position = "bottom_left";
-        };
-
-        osd = {
-          background_opacity = 0.8;
-          lock_keys = false;
-          monitors = [ "eDP-1" ];
-          offset_y = 16;
-          orientation = "horizontal";
-          position = "top_center";
-        };
-
-        shell = {
-          font_family = "Adwaita Mono";
-
-          panel = {
-            attach_clipboard = false;
-            attach_control_center = false;
-            attach_launcher = false;
-            attach_session = false;
-            attach_wallpaper = false;
-            clipboard_placement = "auto";
-            control_center_placement = "floating";
-            floating_offset = 16;
-            launcher_position = "auto";
-            polkit_position = "auto";
-            session_placement = "floating";
-            transparency_mode = "soft";
-            wallpaper_placement = "floating";
-          };
-
-          shadow = {
-            alpha = 1.0;
-            direction = "center";
-          };
-        };
-
-        theme = {
-          mode = "dark";
-          source = "wallpaper";
-          wallpaper_scheme = "m3-content";
-
-          templates = {
-            builtin_ids = [
-              "gtk3"
-              "gtk4"
-              "kcolorscheme"
-              "qt"
-            ] ++(
-              if (config.programs.wezterm.enable) then
-                [ "wezterm" ]
-              else
-                []
-            ) ++(
-              if (desktop.noctalia.compositor == "hyprland") then
-                [ "hyprland" ]
-              else if (desktop.noctalia.compositor == "niri") then
-                [ "niri" ]
-              else
-                []
-            );
-            community_ids = (
-              if (desktop.enableTelegram) then
-                [ "telegram" ]
-              else
-                []
-            ) ++(
-              if (config.devlive.programs.vscode.enable) then
-                [ "vscode" ]
-              else
-                []
-            ) ++(
-              if (config.devlive.programs.yazi.enable) then
-                [ "yazi" ]
-              else
-                []
-            ) ++(
-              if (config.devlive.programs.zathura.enable) then
-                [ "zathura" ]
-              else
-                []
-            ) ++(
-              if (config.devlive.programs.zed-editor.enable) then
-                [ "zed" ]
-              else
-                []
-            );
-          };
-        };
-
-        wallpaper = {
-          directory = "~/Pictures/Wallpapers";
-
-          default = {
-            path = "${config.home.homeDirectory}/Pictures/Wallpapers/wallhaven_j38o75.jpg";
-          };
-        };
-
-        weather = {
-          auto_locate = true;
-        };
-
-        widget = {
-          control-center = {
-            glyph = "snowflake";
-          };
-          cpu = {
-            show_label = false;
-          };
-          network = {
-            show_label = false;
-          };
-          network_rx = {
-            show_label = false;
-          };
-          network_tx = {
-            show_label = false;
-          };
-          ram = {
-            show_label = false;
-          };
-          temp = {
-            show_label = false;
-          };
-          workspaces = {
-            capsule_padding = 8.0;
-          };
-        };
-      };
-    };
     # Enable zen browser transparency and custom layout
-    programs.zen-browser.profiles.default.settings = lib.mkIf config.devlive.programs.zen-browser.enable {
-      "browser.tabs.inTitlebar" = 0;
-      "zen.view.compact.hide-tabbar" = true;
-      "zen.view.compact.hide-toolbar" = false;
-      "zen.view.grey-out-inactive-windows" = false;
-      "zen.widget.linux.transparency" = true;
-    };
+    programs.zen-browser.profiles.default.settings =
+      lib.mkIf config.devlive.programs.zen-browser.enable
+        {
+          "browser.tabs.inTitlebar" = 0;
+          "zen.view.compact.hide-tabbar" = true;
+          "zen.view.compact.hide-toolbar" = false;
+          "zen.view.grey-out-inactive-windows" = false;
+          "zen.widget.linux.transparency" = true;
+        };
 
     services.flameshot = lib.mkIf (desktop.noctalia.compositor == "hyprland") {
       enable = true;
